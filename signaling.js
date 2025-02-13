7cd65f7470394af6b9621f651cfcacf152535fc5
kobatan <emukoba777@gmail.com>
2025-02-13 14:33:37 +0900

    up

--- signaling.js
+++ signaling.js
@@ -9,7 +9,7 @@ wsServer.on("connection", function (ws) {
   console.log("-- websocket connected --");
   ws.on("message", function (message, isBinary) {
       wsServer.clients.forEach(function each(client) {
-          if (isSame(ws, client)) {
+          if (ws === client) {
               console.log("- skip sender -");
           } else {
               client.send(message, { binary: isBinary });
