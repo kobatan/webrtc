e6e06af523e8e3d26aaf7f451318a1c574f77cca
kobatan <emukoba777@gmail.com>
2025-02-14 17:59:24 +0900

    u

--- main.js
+++ main.js
@@ -1,49 +1,50 @@
 const { nowInSec, SkyWayAuthToken, SkyWayContext, SkyWayRoom, SkyWayStreamFactory, uuidV4 } = skyway_room;
 
 
+// トークンの生成
 const token = new SkyWayAuthToken({
-jti: uuidV4(),
-iat: nowInSec(),
-exp: nowInSec() + 60 * 60 * 24,
-scope: {
-    app: {
-    id: "af466ae1-3762-476f-9fb6-11b8da001618",
-    turn: true,
-    actions: ['read'],
-    channels: [
-        {
-        id: '*',
-        name: '*',
-        actions: ['write'],
-        members: [
-            {
-            id: '*',
-            name: '*',
-            actions: ['write'],
-            publication: {
-                actions: ['write'],
-            },
-            subscription: {
-                actions: ['write'],
-            },
-            },
-        ],
-
-        sfuBots: [
-            {
-            actions: ['write'],
-            forwardings: [
-                {
-                actions: ['write'],
-                },
-            ],
-            },
-        ],
-        },
-    ],
-    },
-},
-}).encode("TgUPJ9P/ceQBSBzSV5vmieIVHJ9jtjaScn9w29PrxAE=");
+  jti: uuidV4(),
+  iat: nowInSec(),
+  exp: nowInSec() + 60 * 60 * 24,
+  scope: {
+      app: {
+      id: "af466ae1-3762-476f-9fb6-11b8da001618", // SkyWayのアプリケーションID
+      turn: true,
+      actions: ['read'],
+      channels: [
+          {
+          id: '*',
+          name: '*',
+          actions: ['write'],
+          members: [
+              {
+              id: '*',
+              name: '*',
+              actions: ['write'],
+              publication: {
+                  actions: ['write'],
+              },
+              subscription: {
+                  actions: ['write'],
+              },
+              },
+          ],
+  
+          sfuBots: [
+              {
+              actions: ['write'],
+              forwardings: [
+                  {
+                  actions: ['write'],
+                  },
+              ],
+              },
+          ],
+          },
+      ],
+      },
+  },
+  }).encode("TgUPJ9P/ceQBSBzSV5vmieIVHJ9jtjaScn9w29PrxAE=");  // SkyWayのシークレットキー
 
 document.addEventListener('DOMContentLoaded', function() {
     const videoToggle = document.getElementById('videoToggle');
@@ -66,18 +67,15 @@ document.addEventListener('DOMContentLoaded', function() {
 const localVideo = document.getElementById('local-video');
 const buttonArea = document.getElementById('button-area');
 const remoteMediaArea = document.getElementById('remote-media-area');
-const channelNameInput = document.getElementById('channel-name');
-
+const roomNameInput = document.getElementById('room-name');
 const dataStreamInput = document.getElementById('data-stream');
-
 const myId = document.getElementById('my-id');
 const joinButton = document.getElementById('join');
 const writeButton = document.getElementById('write');
 
-const { audio, video } =
-    await SkyWayStreamFactory.createMicrophoneAudioAndCameraStream();
-video.attach(localVideo);
-await localVideo.play();
+const { audio, video } = await SkyWayStreamFactory.createMicrophoneAudioAndCameraStream();  // マイクとカメラのストリームを取得
+video.attach(localVideo); // ローカルビデオにカメラのストリームをアタッチ
+await localVideo.play();  // ローカルビデオを再生
 
 const data = await SkyWayStreamFactory.createDataStream();
 writeButton.onclick = () => {
@@ -86,33 +84,34 @@ writeButton.onclick = () => {
 };
 
 joinButton.onclick = async () => {
-    if (channelNameInput.value === '') return;
+    if (roomNameInput.value === '') return;
 
-    const context = await SkyWayContext.Create(token);
-    const channel = await SkyWayRoom.FindOrCreate(context, {
-    type: 'p2p',
-    name: channelNameInput.value,
+    const context = await SkyWayContext.Create(token);  // 先ほど作ったトークンからSkyWayのコンテキストを作成
+    const room = await SkyWayRoom.FindOrCreate(context, {  // ルームが無ければ作成、あれば取得
+      type: 'p2p',
+      name: roomNameInput.value,
     });
-    const me = await channel.join();
+    const me = await room.join();  // room に入室します。 すると Member オブジェクトが返ってきます。ここでは me という変数名とします。
+    myId.textContent = me.id; // 自分のIDを表示
 
-    myId.textContent = me.id;
+    await me.publish(audio);  // マイクのストリームを公開
+    await me.publish(video);  // カメラのストリームを公開
+    await me.publish(data);   // データストリームを公開
+    
+    const subscribeAndAttach = (publication) => { // 受信した（購読）時の処理
+    if (publication.publisher.id === me.id) return; // 自分の発行したものは無視
 
-    await me.publish(audio);
-    await me.publish(video);
-    await me.publish(data);
+      const subscribeButton = document.createElement('button'); // 購読ボタンを作成
+      subscribeButton.id = `subscribe-button-${publication.id}`;
+      subscribeButton.textContent = `${publication.publisher.id}: ${publication.contentType}`;
+      
+      buttonArea.appendChild(subscribeButton);
 
-    const subscribeAndAttach = (publication) => {
-    if (publication.publisher.id === me.id) return;
-
-    const subscribeButton = document.createElement('button');
-    subscribeButton.textContent = `${publication.publisher.id}: ${publication.contentType}`;
-    buttonArea.appendChild(subscribeButton);
-
-    subscribeButton.onclick = async () => {
-        const { stream } = await me.subscribe(publication.id);
+      subscribeButton.onclick = async () => { // 購読ボタンが押された時の処理
+        const { stream } = await me.subscribe(publication.id);  // 購読
 
         switch (stream.contentType) {
-        case 'video':
+          case 'video': // videoの場合
             {
             const elm = document.createElement('video');
             elm.playsInline = true;
@@ -121,7 +120,7 @@ joinButton.onclick = async () => {
             remoteMediaArea.appendChild(elm);
             }
             break;
-        case 'audio':
+          case 'audio': // audioの場合
             {
             const elm = document.createElement('audio');
             elm.controls = true;
@@ -130,19 +129,19 @@ joinButton.onclick = async () => {
             remoteMediaArea.appendChild(elm);
             }
             break;
-        case 'data': {
+          case 'data': {  // dataの場合
             const elm = document.createElement('div');
             remoteMediaArea.appendChild(elm);
             elm.innerText = 'data\n';
             stream.onData.add((data) => {
             elm.innerText += data + '\n';
             });
+          }
         }
-        }
-    };
+      };
     };
 
-    channel.publications.forEach(subscribeAndAttach);
-    channel.onStreamPublished.add((e) => subscribeAndAttach(e.publication));
-};
-})();
+    room.publications.forEach(subscribeAndAttach);
+    room.onStreamPublished.add((e) => subscribeAndAttach(e.publication));
+  };
+});
